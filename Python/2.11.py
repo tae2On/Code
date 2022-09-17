b61a657c483709979cb0ba428185cbb76df18de6
tae2On <djs99870@naver.com>
2022-09-18 01:55:32 +0900

    Create 2.11.py

---
+++ Python/2.11.py
@@ -0,0 +1,5 @@
+print('It`s really hot!')
+print('I said "Hello!" to him.')
+print('He said "What`s there?"')
+print('Newlne character is \\n, Tab character is \\t.')
+print('Working directory is C:\\workspace\\mywork.')
