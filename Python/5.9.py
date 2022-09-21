e845064764feca8422a5bdd66919b1b70a6cb2e6
tae2On <djs99870@naver.com>
2022-09-21 22:11:24 +0900

    Update 5.9.py

--- Python/5.9.py
+++ Python/5.9.py
@@ -3,7 +3,7 @@ count = 0
 
 while True : 
     if (data > 0): 
-        data = int(input("정수를 입력하시오 : "))
+        data = [str, input("정수를 입력하시오 : ")]        
         count = count + 1    
     elif (data == -99): 
         break 
@@ -18,4 +18,4 @@ while (data > 0):
     if (b < data):
         data = b    
 
-print(count, "개의 유효한 정수 중 가장 큰 정수는", a, "이고, 가장 작은 정수는", b,  )
+print(count, "개의 유효한 정수 중 가장 큰 정수는", a, "이고, 가장 작은 정수는", b)
