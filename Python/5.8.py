276d4b367b899b10ba0848ba8a1de098c2502ed4
tae2On <djs99870@naver.com>
2022-09-22 01:14:40 +0900

    Update 5.8.py

--- Python/5.8.py
+++ Python/5.8.py
@@ -1,12 +1,14 @@
 while True:
     data = str(input("정수를 입력하시오 : "))
+    print(data)
     data_list = list(data)
     data_list.reverse() 
-    print(''.join(data_list))
+    ''.join(s_list)
 
-    if (data_list == data ):
-        print( data, '은(는) 거꾸로 정수입니다.')
-    elif (data_list == -99): 
+    if (data == data_list):
+        print(data, '은(는) 거꾸로 정수입니다.')
+    elif (data == '-99'): 
         print('프로그램을 종료합니다.')
+        break
     else :
         print(data, '은(는) 거꾸로 정수가 아닙니다.') 
