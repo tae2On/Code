cdc1cedcb946174be395cecb385dfbd36ad86897
tae2On <djs99870@naver.com>
2022-09-24 17:02:06 +0900

    Create 6.8.py

---
+++ Python/6.8.py
@@ -0,0 +1,10 @@
+import math
+def distance(x1, y1, x2, y2): 
+    d = math.sqrt((x2 - x1) **2 + (y2 - y1) ** 2 ) 
+    return d
+    
+x1 = float(input("x1 = "))
+y1 = float(input("y1 = "))
+x2 = float(input("x2 = "))
+y2 = float(input("y2 = "))
+print("(", x1, ",", y1, ")과", "(", x2, ",", y2, ") 사이의 거리는", distance(x1, y1, x2, y2))
