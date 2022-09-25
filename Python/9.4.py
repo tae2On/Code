29ca9165f909d6b972dcbd11813ccc87afc19dc5
tae2On <djs99870@naver.com>
2022-09-25 21:55:32 +0900

    Create 9.4.py

---
+++ Python/9.4.py
@@ -0,0 +1,14 @@
+a = 'ABCD'
+b = '1234'
+new_str1 = ' '
+new_str2 = ' '
+
+for i in range(len(a)): 
+    new_str1 = new_str1 + a[i] + b[i] 
+for i in range(len(a)): 
+    new_str2 = new_str2 + a[i] + (b[::-i])
+    
+print('a = ', a)
+print('b = ', b)
+print('new_str1 = ', new_str1)
+print('new_str2 = ', new_str2)
