b2408e5b63b4dbf0868f5f807211e46a48ae56b1
tae2On <djs99870@naver.com>
2022-09-25 23:19:36 +0900

    Create 9.6.py

---
+++ Python/9.6.py
@@ -0,0 +1,15 @@
+s = '문장 s : English = 89, Science = 90, Math = 92, History = 80'
+print(s)
+
+
+add = 0
+average = 0
+
+for i in s :
+    if i.isnumeric():
+        add = add + int(i)
+        average = average + 1
+
+
+print('총점 : ', add)
+print('평균점수 : ', add / average)
