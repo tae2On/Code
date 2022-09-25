941ace6eea5c83799523b33b9d61048f88d0c0de
tae2On <djs99870@naver.com>
2022-09-25 18:11:47 +0900

    Create 9.2.py

---
+++ Python/9.2.py
@@ -0,0 +1,12 @@
+data = str(input("문자열을 입력하시오 : "))
+
+Alpabet = ' '
+alpabet = ' '
+
+for i in data : 
+    if (i >= 'A' and i <= 'Z'):
+        Alpabet += i
+    elif (i >= 'a' and i <= 'z'):
+        alpabet += i
+    
+print('수정된 문자열 : ', alpabet + Alpabet)
