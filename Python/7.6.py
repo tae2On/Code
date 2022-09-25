4e686cb68947e322dc9b70d9c43212a4928358c1
tae2On <djs99870@naver.com>
2022-09-25 21:56:51 +0900

    Update 7.6.py

--- Python/7.6.py
+++ Python/7.6.py
@@ -3,6 +3,6 @@ data = str(input('문자열을 입력하세요 : '))
 for i in range(len(data) + 1): 
     print(data[0:i])
 
-for i in range(len(data).1.-1):
-    print(data[0:i- 1])
+for i in range(len(data), 1, -1):
+    print(data[0:i - 1])
 
