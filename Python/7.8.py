928bf6afc942ca664d1ef808a3a5631b28ef921e
tae2On <djs99870@naver.com>
2022-09-25 20:49:02 +0900

    Create 7.8.py

---
+++ Python/7.8.py
@@ -0,0 +1,11 @@
+while True : 
+    n = int(input("n을 입력하시오 : "))
+    if (0 < n < 10 ): 
+        for i in range(n) :
+            data = []
+            for j in range(n): 
+                data.append((n ** 2) + 1) 
+        results.append(data)
+
+print(data)
+            
