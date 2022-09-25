a5fb47bb75c3a296cbc78b4c96f1417d1a9f57cb
tae2On <djs99870@naver.com>
2022-09-25 21:56:54 +0900

    Update 7.8.py

--- Python/7.8.py
+++ Python/7.8.py
@@ -1,11 +1,17 @@
-while True : 
-    n = int(input("n을 입력하시오 : "))
-    if (0 < n < 10 ): 
-        for i in range(n) :
-            data = []
-            for j in range(n): 
-                data.append((n ** 2) + 1) 
-        results.append(data)
+num = int(input('n을 입력하시오 : '))
 
-print(data)
-            
+num_list = list(range(1, num*num+1))
+
+for i in range(num) :
+    a = num_list[i*num : (i+1)*num]
+
+    if (i % 2 == 1) :
+        a.reverse()
+    num_list[i*num : (i+1)*num] = a
+
+k = 0
+for i in num_list :
+    print('{:4d}'.format(i), end = '')
+    k += 1
+    if (k % num == 0) :
+        print()
