fc45a29de4b36ea95bf21985e4a00061f9f41088
tae2On <djs99870@naver.com>
2022-09-30 20:18:45 +0900

    Update 9.6.py

--- Python/9.6.py
+++ Python/9.6.py
@@ -1,11 +1,13 @@
+import re 
+
 s = '문장 s : English = 89, Science = 90, Math = 92, History = 80'
 print(s)
-
+data = re.findall("\d+", s)
 
 add = 0
 average = 0
 
-for i in s :
+for i in data :
     if i.isnumeric():
         add = add + int(i)
         average = average + 1
