abbfc35451cc69a19dc89b9db2d81c9d551bdbc6
tae2On <djs99870@naver.com>
2022-09-30 20:18:32 +0900

    Update 9.4.py

--- Python/9.4.py
+++ Python/9.4.py
@@ -6,8 +6,9 @@ new_str2 = ' '
 for i in range(len(a)): 
     new_str1 = new_str1 + a[i] + b[i] 
 for i in range(len(a)): 
-    new_str2 = new_str2 + a[i] + (b[::-i])
-    
+    c = b[::-1]
+    new_str2 = new_str2 + a[i] + (c[i])
+
 print('a = ', a)
 print('b = ', b)
 print('new_str1 = ', new_str1)
